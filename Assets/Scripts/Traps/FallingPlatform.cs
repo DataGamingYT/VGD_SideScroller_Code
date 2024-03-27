489c7d745309757f8e24bf39e73ab97493c8ea28
「 」Data <rylanhughey@gmail.com>
2024-03-27 14:10:57 -0500

    Adding Falling Platforms

---
+++ Assets/Scripts/Traps/FallingPlatform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float fallDelay = 2.0f;
+
+    void OnCollisionEnter(Collision collidedWithThis)
+    {
+        if (collidedWithThis.gameObject.name == "Sphere")
+        {
+            StartCoroutine(FallAfterDelay());
+        }
+    }
+
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
